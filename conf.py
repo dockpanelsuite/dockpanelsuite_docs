3bbb3184119c6fc190702f79cbb22eecda491b4c
Lex Li <support@lextm.com>
2020-01-13 10:14:53 -0500

    Disabled edit_on_github.py.

--- conf.py
+++ conf.py
@@ -31,8 +31,7 @@ sys.path.insert(0, os.path.abspath('_ext'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.intersphinx',
-    'edit_on_github'
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -385,10 +384,6 @@ feed_skip_regex = '(.)*index'
 feed_base_url = 'http://docs.dockpanelsuite.com'
 feed_description = 'DockPanel Suite Documentation'
 feed_author = 'Ryan Rastedt, Lex Li, and others'
-
-if not on_rtd:
-    edit_on_github_project = 'dockpanelsuite/dockpanelsuite_docs'
-    edit_on_github_branch = 'master'
 
 def setup(app):
     on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
